d833206dad285ec191c9cd69bf9c1a5f7b578b6f
HJLee <hjlee2499@gmail.com>
2025-11-04 18:23:39 +0900

    일단 1차 수정-home html, css, js // js 스크롤 이벤트 하나 빼기(수정하기)

--- js/script.js
+++ js/script.js
@@ -1,18 +1,38 @@
+// $(document).ready(function () {
+//   $(".scroll-down").on("click", function () {
+//     $("html, body").animate(
+//       {
+//         scrollTop: $(".main").height(),
+//       },
+//       800
+//     );
+//   });
+// });
 
-const tabs = document.querySelectorAll('.tab');
-const contents = document.querySelectorAll('.content-item');
+const scrollBtn = document.querySelector('.scroll-down');
 
-tabs.forEach(tab => {
-  tab.addEventListener('click', function() {
-    const targetTab = this.getAttribute('data-tab');
-    
-    tabs.forEach(t => t.classList.remove('active'));
-    this.classList.add('active');
-    
-    contents.forEach(content => {
-      content.classList.remove('active');
-      if (content.getAttribute('data-content') === targetTab) {
-        content.classList.add('active');
+scrollBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: window.innerHeight,
+    behavior: 'smooth'
+  });
+});
+
+
+const tabs = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".content-item");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", function () {
+    const targetTab = this.getAttribute("data-tab");
+
+    tabs.forEach((t) => t.classList.remove("active"));
+    this.classList.add("active");
+
+    contents.forEach((content) => {
+      content.classList.remove("active");
+      if (content.getAttribute("data-content") === targetTab) {
+        content.classList.add("active");
       }
     });
   });
